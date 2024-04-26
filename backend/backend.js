b55b3dda5543cf619e96da9d0401706da4e39f48
Ben Christiansen <bwc1@iastate.edu>
2024-04-25 23:58:31 -0500

    List and delete functions working

--- backend/backend.js
+++ backend/backend.js
@@ -18,61 +18,62 @@ const client = new MongoClient(url);
 const db = client.db(dbName);
 
 app.get("/listProducts", async (req, res) => {
-  // await client.connect();
-  // console.log("Node connected successfully to GET MongoDB");
-  // const query = {};
-  // const results = await db.collection("robot").find(query).limit(100).toArray();
-  // console.log(results);
-  // res.status(200);
-  // res.send(results);
+  await client.connect();
+  console.log("Node connected successfully to GET MongoDB");
+  const query = {};
+  const results = await db.collection("fakestore_catalog").find(query).limit(100).toArray();
+  console.log(results);
+  res.status(200);
+  res.send(results);
 });
 
 app.post("/addProduct", async (req, res) => {
-  // try {
-  //   await client.connect();
-  //   const keys = Object.keys(req.body);
-  //   const values = Object.values(req.body);
+  try {
+    await client.connect();
+    const keys = Object.keys(req.body);
+    const values = Object.values(req.body);
 
-  //   const newDocument = {
-  //     // "id": values[0],
-  //     // "name": values[1],
-  //     // "price": values[2],
-  //     // "description": values[3],
-  //     // "imageUrl": values[4],
-  //     "id": req.body.id,
-  //     "name": req.body.name,
-  //     "price": req.body.price,
-  //     "description": req.body.description,
-  //     "imageUrl": req.body.imageUrl,
-  //   };
-  //   console.log(newDocument);
+    const newDocument = {
+      // "id": values[0],
+      // "name": values[1],
+      // "price": values[2],
+      // "description": values[3],
+      // "imageUrl": values[4],
+      "id": req.body.id,
+      "title": req.body.title,
+      "price": req.body.price,
+      "description": req.body.description,
+      "category": req.body.category,
+      "image": req.body.image,
+    };
+    console.log(newDocument);
 
-  //   const results = await db.collection("robot").insertOne(newDocument);
-  //   res.status(200);
-  //   res.send(results);
-  // } catch (error) {
-  //   console.error("An error occurred:", error);
-  //   res.status(500).send({ error: "An internal server error occurred" });
-  // }
+    const results = await db.collection("fakestore_catalog").insertOne(newDocument);
+    res.status(200);
+    res.send(results);
+  } catch (error) {
+    console.error("An error occurred:", error);
+    res.status(500).send({ error: "An internal server error occurred" });
+  }
 });
 
 app.delete("/deleteProduct/:id", async (req, res) => {
-  // try {
-  //   const id = Number(req.params.id);
-  //   await client.connect();
-  //   console.log("Robot to delete :", id);
-  //   const query = { id: id };
-  //   // read data from robot to delete to send it to frontend
-  //   const robotDeleted = await db.collection("robot").findOne(query);
-  //   res.send(robotDeleted);
-  //   // delete
-  //   const results = await db.collection("robot").deleteOne(query);
-  //   res.status(200);
-  //   res.send(results);
-  // } catch (error) {
-  //   console.error("Error deleting robot:", error);
-  //   res.status(500).send({ message: "Internal Server Error" });
-  // }
+  try {
+    const id = Number(req.params.id);
+    await client.connect();
+    console.log("Product to delete :", id);
+    const query = { id: id };
+    // read data from product to delete to send it to frontend
+    const productDeleted = await db.collection("fakestore_catalog").findOne(query);
+    res.send(productDeleted);
+    // delete
+    const results = await db.collection("fakestore_catalog").deleteOne(query);
+    res.status(200);
+    res.send(results);
+  } catch (error) {
+    console.error("Error deleting product:", error);
+    res.status(500).send({ message: "Internal Server Error" });
+  }
 });
 
 app.put("/updateProduct/:id", async (req, res) => {
--- frontend/src/frontend.js
+++ frontend/src/frontend.js
@@ -1,43 +1,207 @@
-fetch("http://localhost:8081/listProducts")
-  .then((response) => response.json())
-  .then((products) => loadProducts(products));
+import { useState, useEffect } from "react";
+function App() {
+  const [product, setProduct] = useState([]);
+  const [oneProduct, setOneProduct] = useState([]);
+  const [oneCategory, setCategory] = useState([]);
+  const [delProd, deleteProduct] = useState([]);
+
+  // new Product
+  const [addNewProduct, setAddNewProduct] = useState({
+    id: 0,
+    title: "",
+    price: 0.0,
+    description: "",
+    category: "",
+    image: "",
+    rating: 0.0,
+  });
+
+  const [viewer1, setViewer1] = useState(false);
+  const [viewer2, setViewer2] = useState(false);
+  const [viewer3, setViewer3] = useState(false);
+
+  useEffect(() => {
+    getAllProducts();
+  }, []);
+  function getAllProducts() {
+    fetch("http://localhost:8081/listProducts")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Show Catalog of Products :");
+        console.log(data);
+        setProduct(data);
+      });
+    setViewer1(!viewer1);
+  }
+
+  const showAllItems = product.map((el) => (
+    <div key={el.id}>
+      <img src={el.image} width={30} alt="images" /> <br />
+      Title: {el.title} <br />
+      Category: {el.category} <br />
+      Price: {el.price} <br />
+    </div>
+  ));
+
+  // function getOneProduct(id) {
+  //   console.log(id);
+  //   if (id >= 1 && id <= 20) {
+  //     fetch("http://127.0.0.1:4000/catalog/" + id)
+  //       .then((response) => response.json())
+  //       .then((data) => {
+  //         console.log("Show one product :", id);
+  //         console.log(data);
+  //         setOneProduct(data);
+  //       });
+  //     if (false === viewer2) setViewer2(true);
+  //   } else {
+  //     console.log("Wrong number of Product id.");
+  //   }
+  // }
+
+  // const showOneItem = oneProduct.map((el) => (
+  //   <div key={el.id}>
+  //     <img src={el.image} width={30} alt="images" /> <br />
+  //     Title: {el.title} <br />
+  //     Category: {el.category} <br />
+  //     Price: {el.price} <br />
+  //     Rating: {el.rating} <br />
+  //   </div>
+  // ));
+
+  // function getCategory(category) {
+  //   console.log(category);
+  //   fetch("http://127.0.0.1:4000/catalog/" + category)
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       console.log("Show category :", category);
+  //       console.log(data);
+  //       setCategory(data);
+  //     });
+  //   if (false === viewer3) setViewer3(true);
+  // }
+
+  // const showCategory = oneCategory.map((el) => (
+  //   <div key={el.category}>
+  //     <img src={el.image} width={30} alt="images" /> <br />
+  //     Title: {el.title} <br />
+  //     Category: {el.category} <br />
+  //     Price: {el.price} <br />
+  //     Rating: {el.rating} <br />
+  //   </div>
+  // ));
+
   
-function loadProducts(products) {
-//   var CardRobot = document.getElementById("col");
+  function newProduct() {
+    fetch("http://localhost:8081/", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            id: 99,
+            title: "Robot Abraham ALDACO-GASTELUM",
+            price: 100.9,
+            description: "I robot is one example of an image for my exercise",
+            category: "hello",
+            image: "https://robohash.org/Abraham",
+        }),
+    })
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then((addNewProduct) => {
+        setAddNewProduct(addNewProduct);
+    })
+    .catch((error) => {
+        console.error('There was a problem with the fetch operation:', error);
+    });
+}
+
+function deleteProd() {
+  // Fetch the value from the input field
+  let id = document.getElementById("deleteProductbyId").value;
+  console.log(id);
+  fetch(`http://localhost:8081/deleteProduct/${id}`, {
+    method: "DELETE",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify({ id: id })
+  })
+    .then((response) => response.json())
+    .then((delProd) => {
+      deleteProduct(delProd);
+    });
+}
+
+
+  return (
+    <div>
+      <h1>Catalog of Products</h1>
+      <div>
+        <h3>Show all available Products.</h3>
+        <button onClick={() => getAllProducts()}>Show All ...</button>
+        {viewer1 && showAllItems}
+      </div>
+
+      <div>
+        <h3>Add New Product</h3>
+        <button onClick={() => newProduct()}>New Product</button>
+      </div>
+
+      <div>
+        <h3>Delete Product</h3>
+        <input type="number" id="deleteProductbyId" placeholder="Enter Product ID" />
+        <button onClick={() => deleteProd()}>Delete Product</button>
+      </div>
+    </div>
+  ); // return end
+} // App end
+export default App;
+
+
+// fetch("http://localhost:8081/listProducts")
+//   .then((response) => response.json())
+//   .then((products) => loadProducts(products));
+  
+// function loadProducts(products) {
+//   var CardProduct = document.getElementById("col");
 
 //   var checkboxes = [];
 //   var cards = [];
 
-//   for (var i = 0; i < myRobots.length; i++) {
-//     let id = myRobots[i].id;
-//     let name = myRobots[i].name;
-//     let price = myRobots[i].price;
-//     let description = myRobots[i].description;
-//     let imageUrl = myRobots[i].imageUrl;
+//   for (var i = 0; i < products.length; i++) {
+//     let id = products[i].id;
+//     let title = products[i].title;
+//     let price = products[i].price;
+//     let description = products[i].description;
+//     let category = products[i].category
+//     let image = products[i].image;
 
 //     let checkbox = "checkbox" + i.toString();
 //     let card = "card" + i.toString();
 
-//     let AddCardRobot = document.createElement("div");
+//     let AddCardProduct = document.createElement("div");
 //     // add class = “col” to new division for Bootstrap
-//     AddCardRobot.classList.add("col");
+//     AddCardProduct.classList.add("col");
 //     // create Bootstrap card
-//     AddCardRobot.innerHTML = `
+//     AddCardProduct.innerHTML = `
 //         <input type="checkbox" id=${checkbox} class="form-check-input" checked>
 //         <label for=${checkbox} class="form-check-label">Show Image ${i}</label>
 //         <div id=${card} class="card shadow-sm">
 //         <div class="card shadow-sm">
-//         <img src=${imageUrl} class="card-img-top" alt="..."></img>
+//         <img src=${image} class="card-img-top" alt="..."></img>
 //         <div class="card-body">
-//         <p class="card-text"> <strong>${id}</strong>, ${name} ${price}</p>
+//         <p class="card-text"> <strong>${id}</strong>, ${title} ${price}</p>
 //         <p>${description}</p>
+//         <p>${category}</p>
 //         <div class="d-flex justify-content-between align-items-center">
 //         </div>
 //         </div>
 //         </div>
 //         `;
 //     // append new division
-//     CardRobot.appendChild(AddCardRobot);
+//     CardProduct.appendChild(AddCardProduct);
 
 //     let cbox = document.getElementById(checkbox);
 //     checkboxes.push(cbox);
@@ -62,64 +226,64 @@ function loadProducts(products) {
 //       }
 //     });
 //   });
-}
+// }
 
-function newProduct() {
-//   fetch(`http://localhost:8081/addRobot`, {
-//     method: "POST",
-//     headers: { "content-type": "application/json" },
-//     body: JSON.stringify({
-//       id: 4,
-//       name: "Robot Abraham ALDACO-GASTELUM",
-//       price: 100.9,
-//       description: "I robot is one example of an image for my exercise",
-//       imageUrl: "https://robohash.org/Abraham",
-//     }),
-//   })
-//     .then((response) => response.json())
-//     .then((addThisRobot) => {
-//       addOneRobot(addThisRobot);
-//     });
-}
+// function newProduct() {
+// //   fetch(`http://localhost:8081/addRobot`, {
+// //     method: "POST",
+// //     headers: { "content-type": "application/json" },
+// //     body: JSON.stringify({
+// //       id: 4,
+// //       name: "Robot Abraham ALDACO-GASTELUM",
+// //       price: 100.9,
+// //       description: "I robot is one example of an image for my exercise",
+// //       imageUrl: "https://robohash.org/Abraham",
+// //     }),
+// //   })
+// //     .then((response) => response.json())
+// //     .then((addThisRobot) => {
+// //       addOneRobot(addThisRobot);
+// //     });
+// }
 
-function deleteProduct() {
-//   // Fetch the value from the input field
-//   let id = document.getElementById("deleteRobotById").value;
-//   console.log(id);
-//   fetch(`http://localhost:8081/deleteRobot/${id}`, {
-//     method: "DELETE",
-//     headers: { "content-type": "application/json" },
-//     body: JSON.stringify({ id: id })
-//   })
-//     .then((response) => response.json())
-//     .then((deleteThisRobot) => {
-//       deleteOneRobotById(deleteThisRobot);
-//     });
-}
+// function deleteProduct() {
+// //   // Fetch the value from the input field
+// //   let id = document.getElementById("deleteRobotById").value;
+// //   console.log(id);
+// //   fetch(`http://localhost:8081/deleteRobot/${id}`, {
+// //     method: "DELETE",
+// //     headers: { "content-type": "application/json" },
+// //     body: JSON.stringify({ id: id })
+// //   })
+// //     .then((response) => response.json())
+// //     .then((deleteThisRobot) => {
+// //       deleteOneRobotById(deleteThisRobot);
+// //     });
+// }
 
-function updateProduct() {
-//   // Fetch the value from the input field
-//   let id = document.getElementById("updateRobotById").value;
-//   console.log(id);
-//   fetch(`http://localhost:8081/updateRobot/${id}`, {
-//     method: "PUT",
-//     headers: { "content-type": "application/json" },
-//     body: JSON.stringify({
-//       name: "Robot Abraham ALDACO-GASTELUM",
-//       price: 100.9,
-//       description: "I robot is one example of an image for my exercise",
-//       imageUrl: "https://robohash.org/Abraham",
-//     }),
-//   })
-//     .then((response) => response.json())
-//     .then((updateThisRobot) => {
-//       updateOneRobotById(updateThisRobot);
-//     });
-}
+// function updateProduct() {
+// //   // Fetch the value from the input field
+// //   let id = document.getElementById("updateRobotById").value;
+// //   console.log(id);
+// //   fetch(`http://localhost:8081/updateRobot/${id}`, {
+// //     method: "PUT",
+// //     headers: { "content-type": "application/json" },
+// //     body: JSON.stringify({
+// //       name: "Robot Abraham ALDACO-GASTELUM",
+// //       price: 100.9,
+// //       description: "I robot is one example of an image for my exercise",
+// //       imageUrl: "https://robohash.org/Abraham",
+// //     }),
+// //   })
+// //     .then((response) => response.json())
+// //     .then((updateThisRobot) => {
+// //       updateOneRobotById(updateThisRobot);
+// //     });
+// }
 
-function getData() {
-  var a = document.getElementById("input_id").value;
-  var message = "user entered this value: " + a;
+// function getData() {
+//   var a = document.getElementById("input_id").value;
+//   var message = "user entered this value: " + a;
 
-  document.getElementById("alert").innerHTML = a;
-}
+//   document.getElementById("alert").innerHTML = a;
+// }
